7e4b9f91d6bab2f4a206a9d50b6a0ff900b3eb36
kevin <kevynceledon@gmail.com>
2026-03-31 04:18:00 -0300

    arreglo de errores

--- models/index.js
+++ models/index.js
@@ -4,7 +4,11 @@ import Player from "./player.js";
 //relacion 1:N (uno a muchos)
 //un equipo tiene muchos jugadores, esto añade automaticamente un "teamId" a la tabla players.
 
-Team.hasMany(Player, {foreignKey:"teamId", as: "player"});
+Team.hasMany(Player, {
+    foreignKey:"teamId",
+     as: "player",
+      onDelete: "RESTRICT", //Evita que se borre un equipo si tiene jugadores
+       onUpdate: "CASCADE"}); // si el ID del equipo cambia actualiza a los jugadores
 Player.belongsTo(Team, {foreignKey:"teamId", as: "team"});
 
 export {Team, Player};
